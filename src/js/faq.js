a8b3940672be23acd3a7fd5438bd723c09ab8511
mishokkartoshki <misha@moskalov.com>
2025-08-10 00:52:01 -0400

    fix faq

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,9 +1,4 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
-document.addEventListener('DOMContentLoaded', () => {
-  new Accordion('.accordion-container', {
-    duration: 300,
-    showMultiple: false,
-  });
-});
+const accordion = new Accordion('.accordion-container');
